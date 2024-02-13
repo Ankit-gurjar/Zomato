74ac57229682382672b40213ddafdd0c9eb591a9
Ankit <agdjp56@gmail.com>
2024-02-13 10:34:50 +0530

    image upload and get done

--- server/controllers/imagecontrollers.js
+++ server/controllers/imagecontrollers.js
@@ -1,8 +1,45 @@
 const expressAsyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const { use } = require("../routes/userRoute");
 
-const uploadimage = expressAsyncHandler(async () => {});
+const uploadimage = expressAsyncHandler(async (req, res) => {
+  const { image_url } = req.body;
+  //   console.log(image_url);
+  const userid = req.user.id;
 
-const allimages = expressAsyncHandler(async () => {});
+  //   console.log(req.user);
+  if (!image_url || !userid) {
+    console.log("Invalid data passed into request");
+    return res.sendStatus(400);
+  }
+  let user = await User.findById(userid);
+
+  try {
+    if (user) {
+      user.images.push(image_url);
+      await user.save();
+      res.status(200).json(user);
+    } else {
+      res.status(400);
+      throw new Error("Retry Image is not Uploaded");
+    }
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+const allimages = expressAsyncHandler(async (req, res) => {
+  const userid = req.user.id;
+  const user = await User.findById(userid);
+  if (user) {
+    res.status(200).send({
+      images: user.images,
+    });
+  } else {
+    res.status(400);
+    throw new Error("NO user found");
+  }
+});
 
 module.exports = { uploadimage, allimages };
--- server/routes/imageRoutes.js
+++ server/routes/imageRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const { uploadimage, allimages } = require("../controllers/imagecontrollers");
-// const { protect } = require("../middleware/authmiddleware");
+const { protect } = require("../middleware/authmiddleware");
 
 const router = express.Router();
 
-router.route("/").post(uploadimage).get(allimages);
+router.route("/").post(protect, uploadimage).get(protect, allimages);
 
 module.exports = router;
